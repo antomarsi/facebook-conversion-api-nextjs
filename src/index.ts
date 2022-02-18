bf6387727ad84d2e13af1dcc445ba04bf25448f7
Antônio Marco da Silva <antomarsi@hotmail.com>
2022-02-17 22:47:03 -0300

    added promise and build

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { v4 as uuidv4 } from 'uuid';
-import FBEventType from '../types';
-import debug from './utils/debug';
+import { v4 as uuidv4 } from "uuid";
+import FBEventType from "../types";
+import debug from "./utils/debug";
 
 declare global {
   interface Window {
@@ -14,9 +14,9 @@ declare global {
  * @constructor
  */
 const fbPageView = (): void => {
-  debug('Client Side Event: PageView');
+  debug("Client Side Event: PageView");
 
-  window.fbq('track', 'PageView');
+  window.fbq("track", "PageView");
 };
 
 /**
@@ -25,43 +25,54 @@ const fbPageView = (): void => {
  * @param event
  * @constructor
  */
-const fbEvent = (event: FBEventType): void => {
+const fbEvent = (event: FBEventType): Promise<void> => {
   const eventId = event.eventId ? event.eventId : uuidv4();
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (event.enableStandardPixel) {
+        window.fbq(
+          "track",
+          event.eventName,
+          {
+            content_type: "product",
+            contents: event.products?.map((product) => ({
+              id: product.sku,
+              quantity: product.quantity,
+            })),
+            value: event.value,
+            currency: event.currency,
+          },
+          { eventID: eventId }
+        );
 
-  setTimeout(() => {
-    if (event.enableStandardPixel) {
-      window.fbq('track', event.eventName, {
-        content_type: 'product',
-        contents: event.products?.map((product) => (
-          { id: product.sku, quantity: product.quantity }
-        )),
-        value: event.value,
-        currency: event.currency,
-      }, { eventID: eventId });
+        debug(`Client Side Event: ${event.eventName}`);
+      }
 
-      debug(`Client Side Event: ${event.eventName}`);
-    }
-
-    fetch('/api/fb-events', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        eventName: event.eventName,
-        eventId,
-        emails: event.emails,
-        phones: event.phones,
-        products: event.products,
-        value: event.value,
-        currency: event.currency,
-      }),
-    }).then((response) => {
-      debug(`Server Side Event: ${event.eventName} (${response.status})`);
-    }).catch((error) => {
-      debug(`Server Side Event: ${event.eventName} (${error.status})`);
-    });
-  }, 250);
+      fetch("/api/fb-events", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          eventName: event.eventName,
+          eventId,
+          emails: event.emails,
+          phones: event.phones,
+          products: event.products,
+          value: event.value,
+          currency: event.currency,
+        }),
+      })
+        .then((response) => {
+          resolve();
+          debug(`Server Side Event: ${event.eventName} (${response.status})`);
+        })
+        .catch((error) => {
+          reject();
+          debug(`Server Side Event: ${event.eventName} (${error.status})`);
+        });
+    }, 250);
+  });
 };
 
 export { fbEvent, fbPageView };
